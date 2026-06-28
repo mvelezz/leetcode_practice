af7742a87f76ed57c4bc4de4bc638fae6059b23c
mvelezz <miahvelez7@gmail.com>
2026-06-27 19:56:54 -0600

    Contains duplicate

---
+++ Leetcode_Practice/ContainsDuplicate.cs
@@ -0,0 +1,36 @@
+﻿bool ContainsDuplicate(int[] nums)
+{
+    List<int> checker = new List<int>();
+
+    foreach(int num in nums)
+    {
+        if (checker.Contains(num))
+        {
+            return true;
+        }
+        else
+        {
+            checker.Add(num);
+        }
+    }
+    return false;
+
+}
+
+int[] falseTest = { };
+
+int[] trueTest = { 2, 2, 3, 3, 4, 4, 4, 4,};
+
+bool falseResult = (ContainsDuplicate(falseTest));
+
+bool trueResult = (ContainsDuplicate(trueTest));
+
+Console.BackgroundColor = ConsoleColor.Red;
+
+Console.WriteLine($"False Result {falseResult}");
+
+Console.BackgroundColor = ConsoleColor.Green;
+
+Console.WriteLine($"True Result {trueResult}");
+
+Console.BackgroundColor = ConsoleColor.Black;
